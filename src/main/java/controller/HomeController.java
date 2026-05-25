f25e8502b67070b968a7d4ccce4069ed383cad4f
Shrawani <your-email@example.com>
2026-05-25 15:01:18 +0530

    updated version

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,13 +1,26 @@
-package com.hospital.controller;
+package com.hospital.hospital_management.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.hospital.hospital_management.repository.PatientRepository;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private PatientRepository repository;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model){
+
+        model.addAttribute(
+            "patients",
+            repository.findAll()
+        );
+
         return "index";
     }
 }
